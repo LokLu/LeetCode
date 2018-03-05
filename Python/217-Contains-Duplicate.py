a9c7a3101cbceb9c835e9513d698c3e977f1d187
Lu Kangkang <whohuwho@126.com>
2018-03-05 16:36:17 +0800

    Create 217-Contains-Duplicate.py

---
+++ Python/217-Contains-Duplicate.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        if len(nums)<2:
+            return False
+        else:
+            nums.sort()
+            s = nums[0]
+            for i in range (1,len(nums)):
+                if nums[i]==s:
+                    return True
+                else:
+                    s = nums[i]
+        return False
