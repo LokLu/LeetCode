ca23636faf81982dbf543ff7b9a1c69aeeff5fca
Lu Kangkang <whohuwho@126.com>
2018-03-25 14:39:55 +0800

    Create 219-Contains-Duplicate-II.py

---
+++ Python/219-Contains-Duplicate-II.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        
+        dict = {}
+        
+        for i,v in enumerate (nums):
+            if v in dict and i-dict[v]<=k:
+                return True
+            dict[v]=i
+            
+        return False
