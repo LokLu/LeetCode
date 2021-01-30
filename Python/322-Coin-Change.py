f966a76ce1068e7ef3cc355f0f73cbf81fa69db5
Lu Kangkang <whohuwho1@gmail.com>
2021-01-30 17:51:51 +0800

    Update 322-Coin-Change.py

--- Python/322-Coin-Change.py
+++ Python/322-Coin-Change.py
@@ -22,3 +22,35 @@ class Solution(object):
                 return least_num
         
         return dp(amount)
+
+    
+# With memorey storing computed amount
+class Solution(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        
+        num_dict = dict()
+        
+        def dp(n):
+            if n in num_dict: return num_dict[n]
+            if n<0: return -1
+            if n==0: return 0
+            least_num = float('INF')
+            for coin in coins:
+                tmp = dp(n-coin)
+                if tmp==-1: continue
+                least_num = min(least_num, 1+dp(n-coin))
+                
+            if least_num==float('INF'):
+                num_dict[n]=-1
+                return -1
+            
+            else:
+                num_dict[n]=least_num
+                return least_num
+            
+        return dp(amount)
